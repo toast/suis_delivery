71635fdafb5ee807d66f403ac48eb24e22a2adb9
Nick Bailey <nbailey@default.local>
2012-12-02 19:54:57 +0000

    Initialising project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuisDeliveryMechRails::Application.config.secret_token = 'c4926a35c75dad84718778008aef80009f1f67debfa0265701dd6ad32949a5a897e5e151dd8a16af1dd92f76b6e069a3ae8f45c616faf84b23ac5bb89ad69fd3'
