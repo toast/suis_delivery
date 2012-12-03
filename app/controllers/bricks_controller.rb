964de165172739876de19eea573e6197821aa3ed
Nick Bailey <nbailey@default.local>
2012-12-03 14:38:13 +0000

    Checking whether compass compile is causing heroku issue.

--- app/controllers/bricks_controller.rb
+++ app/controllers/bricks_controller.rb
@@ -2,7 +2,7 @@ class BricksController < ApplicationController
   def index
 	local_filename = Rails.root.join('app', 'assets', 'stylesheets', 'screen.css.scss')
   	file_contents = ""
-  	File.open(local_filename, 'w') {|f| f.write(file_contents) }
+  	# File.open(local_filename, 'w') {|f| f.write(file_contents) }
   	# system("compass compile .")
   end
 
